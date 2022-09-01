fce71e436edde9268b5633d0163fb06cb0310e97
Hariom1711 <2003om1711@gmail.com>
2022-09-01 10:58:54 +0530

    webelight

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './component/Headers';
+import CardsDetails from './component/CardsDetails';
+import Cards from './component/Cards';
+import {Routes,Route} from "react-router-dom";
+
+function App() {
+  return (
+  <>
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart/:id' element={<CardsDetails />} />
+   </Routes>
+  </>
+  );
+}
+
+export default App;
